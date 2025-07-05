56388ab81feab4a3a621f8cbbb4a515f349a592d
Luis Gerardo Tellez Carranza <vtaejk@hotmail.com>
2025-07-05 01:24:22 +0000

    Added code for excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function randomizeNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomizeArray(arrayRandom) {
+  let max = arrayRandom.length - 1;
+  let min = 0;
+  let random = randomizeNumber(min, max);
+  return arrayRandom[random]
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = randomizeArray(who) + " " + randomizeArray(action) + " " + randomizeArray(what) + " " + randomizeArray(when)
 };
